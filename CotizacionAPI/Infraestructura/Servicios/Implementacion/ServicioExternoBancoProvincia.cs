3a86c5ad28d815638adb5632e57742ee3dcf5a3c
Andrea <127679627+andyDeveloperFromArgentina@users.noreply.github.com>
2024-07-23 09:50:05 -0300

    Update ServicioExternoBancoProvincia.cs

--- CotizacionAPI/Infraestructura/Servicios/Implementacion/ServicioExternoBancoProvincia.cs
+++ CotizacionAPI/Infraestructura/Servicios/Implementacion/ServicioExternoBancoProvincia.cs
@@ -1,13 +1,21 @@
-﻿namespace CotizacionAPI.Infraestructura.Servicios.Implementacion;
+namespace CotizacionAPI.Infraestructura.Servicios.Implementacion;
 
 using CotizacionAPI.Infraestructura.Servicios.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 public sealed class ServicioExternoBancoProvincia : IServicioExternoBancoProvincia
 {
+    private string? uri = string.Empty;
+
+    public ServicioExternoBancoProvincia(IConfiguration configuration)
+    {
+        this.uri = configuration.GetSection("urlBancoProvincia").Value;
+    }
+
     public async Task<List<string>?> Cotizacion()
     {
         var httpClient = new HttpClient();
-        var response = await httpClient.GetFromJsonAsync<List<string>>("http://www.bancoprovincia.com.ar/Principal/Dolar");
+        var response = await httpClient.GetFromJsonAsync<List<string>>($"{this.uri}Principal/Dolar");
 
         return response;
     }
